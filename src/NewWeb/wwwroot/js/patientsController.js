915fe6da7719856030ad97fe8201717bb34c81e0
Qlanowski <ulanowskikarol@gmail.com>
2016-09-03 20:58:45 +0200

    Adding Project with some angular 1.5

---
+++ src/NewWeb/wwwroot/js/patientsController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+
+    "use strict";
+    //Getting the existing module
+    angular.module("app-patients")
+        .controller("patientsController", patientsController);
+
+    function patientsController() {
+
+        var vm = this;
+
+        vm.patients = [{
+            Name: "Krzysztof",
+            Surname: "Kapciak"
+        },
+        {
+            Name: "Alek",
+            Surname: "Rokowicz"
+        },
+        {
+            Name: "Beata",
+            Surname: "Walczak"
+        }]
+    }
+
+})();
