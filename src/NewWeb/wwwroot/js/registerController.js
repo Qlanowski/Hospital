7eb81e8b7810753a65363ab5fa41d4e8107cbdd2
Qlanowski <ulanowskikarol@gmail.com>
2016-09-05 11:51:52 +0200

    Working registration form.

---
+++ src/NewWeb/wwwroot/js/registerController.js
@@ -0,0 +1,35 @@
+﻿(function () {
+
+    "use strict";
+    //Getting the existing module
+    angular.module("app-patients")
+        .controller("registerController", registerController);
+
+    function registerController($http) {
+        var vm = this;
+          
+        vm.errorMessage = "";
+          
+        vm.doctors = [];
+
+        vm.newDoctor = {};
+
+        vm.registerDoctor = function () {
+            //alert(vm.newDoctor.UserName)
+
+            $http.post("/Auth/Register", vm.newDoctor)
+                .then(function (response) {
+                    //success
+                    vm.doctors.push(response.data);
+                    vm.newDoctor = {};
+                }, function () {
+                    //failure
+                    vm.errorMessage = "Failed to register";
+                })
+                
+
+        };
+
+    }
+
+})();
